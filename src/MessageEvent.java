7cd23eda1944b986d5fb2e8f2c6ae463a1538a06
ahlamennesyry05 <ahlamennesyry05@gmail.com>
2026-05-09 15:52:20 +0100

    Ajout de la classe MessageEvent

---
+++ src/MessageEvent.java
@@ -0,0 +1,12 @@
+public class MessageEvent {
+
+    private String message;
+    private Bavard auteur;
+
+    public MessageEvent(String message, Bavard auteur) {
+
+        this.message = message;
+        this.auteur = auteur;
+
+    }
+}
