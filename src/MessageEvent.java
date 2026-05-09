507a2a7f8bc4c4b9c02db72b11b491a808a50094
ahlamennesyry05 <ahlamennesyry05@gmail.com>
2026-05-09 20:42:29 +0100

    Update MessageEvent.java

--- src/MessageEvent.java
+++ src/MessageEvent.java
@@ -4,16 +4,30 @@ public class MessageEvent {
     private Bavard auteur;
     private static int compteur = 0;
     private int id;
+    private int bienveillance;
 
     public MessageEvent(String message, Bavard auteur) {
-         this.id = compteur;
-         compteur++;
+        this.id = compteur;
+        compteur++;
 
         this.message = message;
         this.auteur = auteur;
-
+        this.bienveillance = 0;
     }
+
     public int getId() {
-    return id;
-   }
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getBienveillance() {
+        return bienveillance;
+    }
+
+    public void setBienveillance(int bienveillance) {
+        this.bienveillance = bienveillance;
+    }
 }
