d4be9673768bba75babeda72261f25238a7910e1
ahlamennesyry05 <ahlamennesyry05@gmail.com>
2026-05-09 22:35:44 +0100

    Update Bavard.java

--- src/Bavard.java
+++ src/Bavard.java
@@ -1,12 +1,14 @@
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bavard implements MessageListener {
 
     private String nom;
+    private String personnalite;
     private List<Bavard> amis;
     private List<Integer> messagesRecus;
-    private String personnalite;
 
     public Bavard(String nom, String personnalite) {
         this.nom = nom;
@@ -15,16 +17,21 @@ public class Bavard implements MessageListener {
         this.messagesRecus = new ArrayList<>();
     }
 
+    public String getNom() {
+        return nom;
+    }
+
     public void ajouterAmi(Bavard b) {
         amis.add(b);
     }
 
     public void envoyerMessage(String message) {
-        MessageEvent event = new MessageEvent(message, this);
+        MessageEvent event = new MessageEvent(message, this, 5);
 
         System.out.println(nom + " envoie : " + message);
 
         for (Bavard ami : amis) {
+            event.incrementerTransmission();
             ami.recevoirMessage(event);
         }
     }
@@ -35,28 +42,29 @@ public class Bavard implements MessageListener {
         }
 
         messagesRecus.add(event.getId());
+        event.ajouterAuChemin(nom);
 
-       
         if (personnalite.equals("positif")) {
             event.setBienveillance(event.getBienveillance() + 1);
-            System.out.println(nom + " modifie la bienveillance à : " + event.getBienveillance());
-            System.out.println(nom + " 😊 reçoit : " + event.getMessage()
-                    + " (bienveillance : " + event.getBienveillance() + ")");
-        } 
-        else if (personnalite.equals("negatif")) {
+            event.setMessage(event.getMessage().toUpperCase());
+        } else if (personnalite.equals("negatif")) {
             event.setBienveillance(event.getBienveillance() - 1);
-            System.out.println(nom + " modifie la bienveillance à : " + event.getBienveillance());
-            System.out.println(nom + " 😠 reçoit : " + event.getMessage()
-                    + " (bienveillance : " + event.getBienveillance() + ")");
-        } 
-        else {
-            System.out.println(nom + " 😐 reçoit : " + event.getMessage()
-                    + " (bienveillance : " + event.getBienveillance() + ")");
+            event.setMessage(event.getMessage().toLowerCase());
         }
 
-       
-        for (Bavard ami : amis) {
-            ami.recevoirMessage(event);
+        System.out.println(nom + " recoit : " + event.getMessage()
+                + " | personnalite : " + personnalite
+                + " | bienveillance : " + event.getBienveillance()
+                + " | chemin : " + event.getChemin()
+                + " | transmissions : " + event.getCompteurTransmissions());
+
+        if (event.peutEncoreSePropager()) {
+            event.augmenterProfondeur();
+
+            for (Bavard ami : amis) {
+                event.incrementerTransmission();
+                ami.recevoirMessage(event);
+            }
         }
     }
 }
