35d12986a949fa57713b77416c4fcaee77b6364e
ahlamennesyry05 <ahlamennesyry05@gmail.com>
2026-05-09 22:34:44 +0100

    Update MessageEvent.java

--- src/MessageEvent.java
+++ src/MessageEvent.java
@@ -1,18 +1,28 @@
+
 public class MessageEvent {
 
+    private static int compteurId = 0;
+
+    private int id;
     private String message;
     private Bavard auteur;
-    private static int compteur = 0;
-    private int id;
     private int bienveillance;
+    private String chemin;
+    private int compteurTransmissions;
+    private int profondeur;
+    private int profondeurMax;
 
-    public MessageEvent(String message, Bavard auteur) {
-        this.id = compteur;
-        compteur++;
+    public MessageEvent(String message, Bavard auteur, int profondeurMax) {
+        this.id = compteurId;
+        compteurId++;
 
         this.message = message;
         this.auteur = auteur;
         this.bienveillance = 0;
+        this.chemin = auteur.getNom();
+        this.compteurTransmissions = 0;
+        this.profondeur = 0;
+        this.profondeurMax = profondeurMax;
     }
 
     public int getId() {
@@ -23,11 +33,43 @@ public class MessageEvent {
         return message;
     }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Bavard getAuteur() {
+        return auteur;
+    }
+
     public int getBienveillance() {
         return bienveillance;
     }
 
     public void setBienveillance(int bienveillance) {
         this.bienveillance = bienveillance;
+    }
+
+    public String getChemin() {
+        return chemin;
+    }
+
+    public void ajouterAuChemin(String nomBavard) {
+        chemin = chemin + " -> " + nomBavard;
+    }
+
+    public int getCompteurTransmissions() {
+        return compteurTransmissions;
+    }
+
+    public void incrementerTransmission() {
+        compteurTransmissions++;
+    }
+
+    public boolean peutEncoreSePropager() {
+        return profondeur < profondeurMax;
+    }
+
+    public void augmenterProfondeur() {
+        profondeur++;
     }
 }
