00bfe6085d380bf226c27a81a010537566627ead
ahlamennesyry05 <ahlamennesyry05@gmail.com>
2026-05-09 16:42:58 +0100

    Update MessageEvent.java

--- src/MessageEvent.java
+++ src/MessageEvent.java
@@ -2,8 +2,12 @@ public class MessageEvent {
 
     private String message;
     private Bavard auteur;
+    private static int compteur = 0;
+    private int id;
 
     public MessageEvent(String message, Bavard auteur) {
+         this.id = compteur;
+         compteur++;
 
         this.message = message;
         this.auteur = auteur;
