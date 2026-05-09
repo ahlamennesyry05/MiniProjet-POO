9763b9572ce6dbab8856b17847f3584d59bb8c16
ahlamennesyry05 <ahlamennesyry05@gmail.com>
2026-05-09 22:33:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
+
     public static void main(String[] args) {
 
         Bavard alice = new Bavard("Alice", "positif");
         Bavard bob = new Bavard("Bob", "neutre");
         Bavard clara = new Bavard("Clara", "negatif");
+        Bavard david = new Bavard("David", "positif");
 
         alice.ajouterAmi(bob);
         bob.ajouterAmi(clara);
         clara.ajouterAmi(alice);
+        clara.ajouterAmi(david);
 
         alice.envoyerMessage("Bonjour tout le monde !");
     }
-}  
+}
